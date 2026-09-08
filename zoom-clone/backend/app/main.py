02d11ea76c972d8c766b14607d0d529280c1be13
Nishant-Devlal <nishantdevlal2006@gmail.com>
2026-09-08 07:02:11 +0530

    added new and old domain names

--- zoom-clone/backend/app/main.py
+++ zoom-clone/backend/app/main.py
@@ -46,6 +46,7 @@ app.mount(
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://zoom-clone-frontend-three.vercel.app",
     "https://zoom-clone-nishant.vercel.app",
 ]
 
